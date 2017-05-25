4d8727e13548d06608545474077b02402a5d4a18
Mahesh Natarajan <umamags@gmail.com>
2017-05-25 14:15:37 +0530

    war runnable

--- src/main/java/com/capg/cardservices/CardServicesApp.java
+++ src/main/java/com/capg/cardservices/CardServicesApp.java
@@ -3,6 +3,8 @@ package com.capg.cardservices;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,12 +14,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class CardServicesApp {
+public class CardServicesApp extends SpringBootServletInitializer {
 
-	 private static Logger log = Logger.getLogger(CardServicesApp.class);
-    public static void main(String[] args) {
-    	log.info("Entering Main logger enabled");
-        SpringApplication.run(CardServicesApp.class, args);
-    }
+	private static Logger log = Logger.getLogger(CardServicesApp.class);
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CardServicesApp.class);
+	}
+
+	public static void main(String[] args) {
+		log.info("Entering Main logger enabled");
+		SpringApplication.run(CardServicesApp.class, args);
+	}
 }
-
